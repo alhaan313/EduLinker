4da87c5dc14a4b03da4d33c431683ceee75deb3f
alhaan313 <a.md.alhaan@gmail.com>
2025-02-18 07:31:47 +0530

    few change

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from flask import Flask, render_template, request
-from googlesearch import search
-import webbrowser
 from bs4 import BeautifulSoup
 import requests
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+CX = os.getenv('CX')
 
 app = Flask(__name__)
 
@@ -11,32 +16,22 @@ def index():
     return render_template('index.html')
 
 @app.route('/search', methods=['POST'])
-def retrieve_internships():
-    topic = request.form['topic']
-    url = f"https://www.google.com/search?q={topic }+internships"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers)
-    #response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    links = []
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for link in soup.find_all('a'):
-            href = link.get('href')
-            if href and href.startswith('/url?q='):
-                href = href.replace('/url?q=', '')
-                href = href.split('&')[0]
-                if 'http' in href or 'https' in href:
-                    if 'google.com' not in href:  # Exclude links with google.com domain
-                        links.append(href)
-            if len(links) == 5:
-                break
-    else:
-        print("Failed to retrieve search results.")
-    
-    # return links    
+def retrieve():
+    # Get the search type (internship, job, or course) from the form
+    search_type = request.form['search_type']  # Assuming form field is named 'search_type'
+    topic = request.form['topic']  # The user input for the specific topic
 
-    return render_template('results.html', links=links)
+    url = f"https://www.google.com/search?q={topic}+{search_type}"
+    
+    query = f"{topic} {search_type}"
+    url = f"https://www.googleapis.com/customsearch/v1?q={query}&key={API_KEY}&cx={CX}"    
+    response = requests.get(url)
+    # print(response)
+    data = response.json()
+    # print(data)
+    links = [item['link'] for item in data.get('items', [])[:5]]  # Extract top 5 links
+    # print(links)
+    return render_template('results.html', links=links, search_type=search_type)
 
 if __name__ == '__main__':
     app.run(debug=True)
